29e66933ac52e465634d5290ac75a2a57f084e16
Tasneem-Zaareer <tasneem.zareer@gmail.com>
2025-01-14 16:00:43 +0400

    add translation

--- lib/features/ramadan_quiz/views/quiz_welcom_view.dart
+++ lib/features/ramadan_quiz/views/quiz_welcom_view.dart
@@ -1,6 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:ramadan_guide_app/core/constants/app_images.dart';
-
+import 'package:ramadan_guide_app/core/widgets/buttons/custom_button.dart';
+import 'package:ramadan_guide_app/core/widgets/text/custome_text.dart';
 import 'quiz_questions_view.dart';
 
 class RamadanQuizWelcomeView extends StatelessWidget {
@@ -23,76 +25,52 @@ class RamadanQuizWelcomeView extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(
-                'Ramadan Quiz',
-                style: TextStyle(
-                  fontSize: 36,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  shadows: [
-                    Shadow(
-                        color: Colors.black.withOpacity(0.5),
-                        offset: const Offset(2, 2),
-                        blurRadius: 5)
-                  ],
-                ),
+              CustomText(
+                text: tr('ramadanQuiz'),
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                shadows: [
+                  Shadow(
+                      color: Colors.black.withOpacity(0.5),
+                      offset: const Offset(2, 2),
+                      blurRadius: 5)
+                ],
               ),
+
               const SizedBox(height: 10),
-              Text(
-                'Test your knowledge of Ramadan',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white.withOpacity(0.8),
-                ),
+              CustomText(
+                text: tr('testYourKnowledgeOfRamadan'),
+                fontSize: 18,
+                color: Colors.white,
               ),
+
               const SizedBox(height: 30),
               // Start Button
-              ElevatedButton(
+              CustomButton(
+                width: 200,
+                background: Theme.of(context).colorScheme.tertiary,
+                title: tr('startQuiz'),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
                 onPressed: () {
-                  // Navigate to the quiz screen
                   Navigator.push(
                     context,
                     MaterialPageRoute(
                         builder: (context) => const QuizQuestionView()),
                   );
                 },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.amber,
-                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                child: const Text(
-                  'Start Quiz',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
               ),
               const SizedBox(height: 10),
-              Container(
+              CustomButton(
                 width: 200,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // back home
-                    Navigator.pop(context);
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //       builder: (context) => const BottomAppBar()),
-                    // );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.background,
-                    padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: const Text(
-                    'Home',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
+                background: Theme.of(context).colorScheme.background,
+                title: tr('home'),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
             ],
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ramadan_guide_app/features/home/views/home_view.dart';
 import 'package:ramadan_guide_app/features/onboarding/views/onboarding_control.dart';
 import 'core/constants/theme.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      path: 'assets/languages', // Path to the translation files
+      fallbackLocale: const Locale('en'),
+
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,6 +27,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       child: MaterialApp(
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
         theme: lightMode,
         debugShowCheckedModeBanner: false,
         title: 'Ramadan',
