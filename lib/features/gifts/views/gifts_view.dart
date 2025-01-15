03a48aacecc4fbedccc52738f33a35d167ba619e
Tasneem-Zaareer <tasneem.zareer@gmail.com>
2025-01-15 18:46:15 +0400

    add gifts view

--- lib/features/gifts/views/gifts_view.dart
+++ lib/features/gifts/views/gifts_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class GiftsView extends StatelessWidget {
   const GiftsView({super.key});
@@ -6,13 +7,38 @@ class GiftsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Container(
-          height: 200,
-          width: double.infinity,
-          color: Colors.red,
+      body: SafeArea(child: GiftsGridview()),
+    );
+  }
+}
+
+class GiftsGridview extends StatelessWidget {
+  const GiftsGridview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        padding: EdgeInsets.symmetric(horizontal: 20.h, vertical: 10.h),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          crossAxisCount: 2,
+          childAspectRatio: 1 / 1.2,
         ),
-      ]),
+        itemBuilder: (context, index) => GiftContainer());
+  }
+}
+
+class GiftContainer extends StatelessWidget {
+  const GiftContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Theme.of(context).colorScheme.primary,
+      ),
     );
   }
 }
