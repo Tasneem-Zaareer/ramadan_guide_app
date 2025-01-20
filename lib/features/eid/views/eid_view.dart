f33fde50ac45f7ebed4ce697dcf464fafddead44
Tasneem-Zaareer <tasneem.zareer@gmail.com>
2025-01-20 10:19:16 +0400

    edit eid view

--- lib/features/eid/views/eid_view.dart
+++ lib/features/eid/views/eid_view.dart
@@ -1,5 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ramadan_guide_app/core/constants/app_images.dart';
 import 'package:ramadan_guide_app/core/widgets/text/custome_text.dart';
 
 class EidView extends StatelessWidget {
@@ -10,39 +12,39 @@ class EidView extends StatelessWidget {
     final List<Map<String, dynamic>> eidActivityList = [
       {
         'activityName': tr('eidTakbeerat'),
-        'activityImage': '',
-        'activityColor': Theme.of(context).colorScheme.primary,
+        'activityImage': AppImages.girlWithLantern,
+        'activityColor':
+            Theme.of(context).colorScheme.secondary.withOpacity(0.8),
       },
       {
-        'activityName': tr('eidTakbeerat'),
-        'activityImage': '',
-        'activityColor': Theme.of(context).colorScheme.primary,
+        'activityName': tr('eidDua'),
+        'activityImage': AppImages.boyPray,
+        'activityColor':
+            Theme.of(context).colorScheme.secondary.withOpacity(0.6),
       },
       {
-        'activityName': tr('eidTakbeerat'),
-        'activityImage': '',
-        'activityColor': Theme.of(context).colorScheme.primary,
-      },
-      {
-        'activityName': tr('eidTakbeerat'),
-        'activityImage': '',
-        'activityColor': Theme.of(context).colorScheme.primary,
-      },
-      {
-        'activityName': tr('eidTakbeerat'),
-        'activityImage': '',
-        'activityColor': Theme.of(context).colorScheme.primary,
+        'activityName': tr('sadaqahAlfitr'),
+        'activityImage': AppImages.zakat,
+        'activityColor':
+            Theme.of(context).colorScheme.secondary.withOpacity(0.4),
       },
     ];
 
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) => EidActivityWidget(
-          activityName: eidActivityList[index]['activityName'],
-          activityImage: '',
-          activityColor: Theme.of(context).colorScheme.primary,
-        ),
+      body: Column(
+        children: [
+          // Image.asset(AppImages.quiz),
+          Expanded(
+            child: ListView.builder(
+              itemCount: eidActivityList.length,
+              itemBuilder: (context, index) => EidActivityWidget(
+                activityName: eidActivityList[index]['activityName'],
+                activityImage: eidActivityList[index]['activityImage'],
+                activityColor: eidActivityList[index]['activityColor'],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -64,13 +66,25 @@ class EidActivityWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-      height: MediaQuery.of(context).size.height * 0.1,
+      padding: EdgeInsets.only(left: 15.h, right: 15.h, top: 10.h),
+      height: MediaQuery.of(context).size.height * 0.15,
       decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.primary,
+        color: activityColor,
+        borderRadius: BorderRadius.circular(10),
       ),
-      child: CustomText(
-        text: activityName,
+      child: Stack(
+        children: [
+          Positioned(
+            right: 0,
+            child: Image.asset(
+              activityImage,
+              height: MediaQuery.of(context).size.height * 0.16,
+            ),
+          ),
+          CustomText(
+            text: activityName,
+          ),
+        ],
       ),
     );
   }
