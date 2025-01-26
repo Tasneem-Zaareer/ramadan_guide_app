2f2d767f733fdec3eb6c26ccf32d87bd5ddda3b4
Tasneem-Zaareer <tasneem.zareer@gmail.com>
2025-01-26 12:32:31 +0400

    edit quiz list

---
+++ lib/features/ramadan_quiz/views/quiz_level_questions_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class QuizLevelQuestionView extends StatelessWidget {
+  const QuizLevelQuestionView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(child: Text('QuizCategoryView')),
+    );
+  }
+}
--- lib/features/ramadan_quiz/views/quiz_welcom_view.dart
+++ lib/features/ramadan_quiz/views/quiz_welcom_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ramadan_guide_app/core/constants/app_images.dart';
 import 'package:ramadan_guide_app/core/widgets/buttons/custom_button.dart';
 import 'package:ramadan_guide_app/core/widgets/text/custome_text.dart';
+import 'package:ramadan_guide_app/features/ramadan_quiz/views/quiz_level_questions_view.dart';
 import 'quiz_questions_view.dart';
 
 class RamadanQuizWelcomeView extends StatelessWidget {
@@ -11,23 +12,183 @@ class RamadanQuizWelcomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final List<Map<String, dynamic>> quizList = [
-    //   {
-    //     'image': AppImages.umrah,
-    //   }
-    // ];
-
-    final List<String> quizList = [
-      AppImages.mosqueQuiz2,
-      AppImages.dua,
-      AppImages.quranQuiz,
-      AppImages.dates,
-      AppImages.moonQuiz,
-      AppImages.couple,
-      AppImages.mosqueQuiz,
-      AppImages.twoMen,
-      AppImages.mosqueSmall,
-      AppImages.eid,
+    final List<Map<String, dynamic>> quizList = [
+      {
+        'image': AppImages.umrah,
+        'title': tr('levelOne'),
+        'questions': [
+          {
+            'question': tr('question1'),
+            'answers': [
+              tr('answer1'),
+              tr('answer2'),
+              tr('answer3'),
+              tr('answer4'),
+            ],
+            'correctAnswer': 2,
+          },
+        ],
+      },
+      {
+        'image': AppImages.mosqueQuiz2,
+        'title': tr('levelTwo'),
+        'questions': [
+          {
+            'question': tr('question2'),
+            'answers': [
+              tr('answer1'),
+              tr('answer2'),
+              tr('answer3'),
+              tr('answer4'),
+            ],
+            'correctAnswer': 3,
+          },
+        ],
+      },
+      {
+        'image': AppImages.dua,
+        'title': tr('levelThree'),
+        'questions': [
+          {
+            'question': tr('question3'),
+            'answers': [
+              tr('answer1'),
+              tr('answer2'),
+              tr('answer3'),
+              tr('answer4'),
+            ],
+            'correctAnswer': 1,
+          },
+        ],
+      },
+      {
+        'image': AppImages.quranQuiz,
+        'title': tr('levelFour'),
+        'questions': [
+          {
+            'question': tr('question4'),
+            'answers': [
+              tr('answer1'),
+              tr('answer2'),
+              tr('answer3'),
+              tr('answer4'),
+            ],
+            'correctAnswer': 4,
+          },
+        ],
+      },
+      {
+        'image': AppImages.dates,
+        'title': tr('levelFive'),
+        'questions': [
+          {
+            'question': tr('question5'),
+            'answers': [
+              tr('answer1'),
+              tr('answer2'),
+              tr('answer3'),
+              tr('answer4'),
+            ],
+            'correctAnswer': 3,
+          },
+        ],
+      },
+      {
+        'image': AppImages.moonQuiz,
+        'title': tr('levelSix'),
+        'questions': [
+          {
+            'question': tr('question6'),
+            'answers': [
+              tr('answer1'),
+              tr('answer2'),
+              tr('answer3'),
+              tr('answer4'),
+            ],
+            'correctAnswer': 2,
+          },
+        ],
+      },
+      {
+        'image': AppImages.couple,
+        'title': tr('levelSeven'),
+        'questions': [
+          {
+            'question': tr('question7'),
+            'answers': [
+              tr('answer1'),
+              tr('answer2'),
+              tr('answer3'),
+              tr('answer4'),
+            ],
+            'correctAnswer': 4,
+          },
+        ],
+      },
+      {
+        'image': AppImages.mosqueQuiz,
+        'title': tr('levelEight'),
+        'questions': [
+          {
+            'question': tr('question8'),
+            'answers': [
+              tr('answer1'),
+              tr('answer2'),
+              tr('answer3'),
+              tr('answer4'),
+            ],
+            'correctAnswer': 1,
+          },
+        ],
+      },
+      {
+        'image': AppImages.twoMen,
+        'title': tr('levelNine'),
+        'questions': [
+          {
+            'question': tr('question9'),
+            'answers': [
+              tr('answer1'),
+              tr('answer2'),
+              tr('answer3'),
+              tr('answer4'),
+            ],
+            'correctAnswer': 3,
+          },
+        ],
+      },
+      {
+        'image': AppImages.mosqueSmall,
+        'title': tr('levelTen'),
+        'questions': [
+          {
+            'question': tr('question10'),
+            'answers': [
+              tr('answer1'),
+              tr('answer2'),
+              tr('answer3'),
+              tr('answer4'),
+            ],
+            'correctAnswer': 2,
+          },
+        ],
+      },
+      {
+        'image': AppImages.eid,
+        'title': tr('levelEleven'),
+        'questions': [
+          {
+            'question': tr('question11'),
+            'answers': [
+              tr('answer1'),
+              tr('answer2'),
+              tr('answer3'),
+              tr('answer4'),
+            ],
+            'correctAnswer': 4,
+          },
+        ],
+      },
     ];
 
     return Scaffold(
@@ -78,7 +239,8 @@ class RamadanQuizWelcomeView extends StatelessWidget {
                   ),
                   itemBuilder: (context, index) => QuizLevelWidget(
                     levelNum: index + 1,
-                    image: quizList[index],
+                    image: quizList[index]['image'],
+                    questions: quizList[index]['questions'],
                   ),
                 ),
               )
@@ -99,13 +261,7 @@ class RamadanQuizWelcomeView extends StatelessWidget {
                 textColor: Theme.of(context).colorScheme.secondary,
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const QuizQuestionView()),
-                  );
-                },
+                onPressed: () {},
               ),
             ),
           ),
@@ -132,64 +288,74 @@ class QuizLevelWidget extends StatelessWidget {
     super.key,
     required this.levelNum,
     required this.image,
+    required this.questions,
   });
 
   final int levelNum;
   final String image;
+  final List questions;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned(
-          top: 0.h,
-          right: 10.h,
-          child: Image.asset(
-            AppImages.cloud,
-            // fit: BoxFit.cover,
-            height: MediaQuery.of(context).size.height * 0.05,
-            width: MediaQuery.of(context).size.height * 0.05,
-          ),
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const QuizLevelQuestionView(),
         ),
-        Container(
-          height: MediaQuery.of(context).size.height * 0.15,
-          width: MediaQuery.of(context).size.height * 0.15,
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
-            // borderRadius: BorderRadius.circular(10),
-            shape: BoxShape.circle,
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 0.h,
+            right: 10.h,
+            child: Image.asset(
+              AppImages.cloud,
+              // fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height * 0.05,
+              width: MediaQuery.of(context).size.height * 0.05,
+            ),
           ),
-          margin: const EdgeInsets.symmetric(horizontal: 10),
-          child: CustomText(
-            text: levelNum.toString(),
-            fontSize: 12.sp,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+          Container(
+            height: MediaQuery.of(context).size.height * 0.15,
+            width: MediaQuery.of(context).size.height * 0.15,
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+              // borderRadius: BorderRadius.circular(10),
+              shape: BoxShape.circle,
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            child: CustomText(
+              text: levelNum.toString(),
+              fontSize: 12.sp,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
-        ),
-        Positioned(
-          bottom: 0.h,
-          left: 20.h,
-          // right: 0,
-          top: 0,
-          child: Image.asset(
-            image,
-            // fit: BoxFit.cover,
-            height: MediaQuery.of(context).size.height * 0.1,
-            width: MediaQuery.of(context).size.height * 0.08,
+          Positioned(
+            bottom: 0.h,
+            left: 20.h,
+            // right: 0,
+            top: 0,
+            child: Image.asset(
+              image,
+              // fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height * 0.1,
+              width: MediaQuery.of(context).size.height * 0.08,
+            ),
           ),
-        ),
-        Positioned(
-          bottom: 10.h,
-          left: 0.h,
-          child: Image.asset(
-            AppImages.cloud,
-            // fit: BoxFit.cover,
-            height: MediaQuery.of(context).size.height * 0.05,
-            width: MediaQuery.of(context).size.height * 0.05,
+          Positioned(
+            bottom: 10.h,
+            left: 0.h,
+            child: Image.asset(
+              AppImages.cloud,
+              // fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height * 0.05,
+              width: MediaQuery.of(context).size.height * 0.05,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
